2b13cb8d716c5b05013d8fa2f83a7a6f195615e1
Isaac Barcroft <isaacbarcroft@Isaacs-MacBook-Air.local>
2021-08-31 12:47:00 -0400

    Max 3

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -18,7 +18,9 @@ return num2;
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
-
+function maxOfThree(num1, num2, num3){
+  return Math.max(num1, num2, num3);
+}
 
 
   // ---------------------
